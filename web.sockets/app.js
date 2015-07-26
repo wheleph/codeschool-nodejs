10ba8e55a7c2bef810bbfe049934763477536511
wheleph <wheleph@gmail.com>
2015-07-26 17:09:35 +0300

    Implemented simple web socket server

--- web.sockets/app.js
+++ web.sockets/app.js
@@ -3,18 +3,19 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
 io.sockets.on('connection', function(client) {
   console.log("Client connected...");
 
-  client.on('answer', function(question, answer) {
-    client.broadcast.emit('answer', question, answer);
-  });
-
   client.on('question', function(question) {
-    if (!client.question_asked) {
-      client.question_asked = true;
-      client.broadcast.emit('question', question);
-    }
+    console.log('Got question: ' + question);
+    // Send question back to the originator
+    client.emit('question', question);
+    // Send question to the other listeners
+    client.broadcast.emit('question', question);
   });
 });
 
